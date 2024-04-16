0a63e8cef443ad8cf7205c9c98d47b911144033a
Leigh Allen <lallenation@gmail.com>
2024-04-16 11:39:43 +0000

    challenge 6 complete

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react'
 import {savedPosts} from '../posts.json';
 import css from './css/Content.module.css';
 import PostItem from "./PostItem";
+import Loader from "./Loader";
 
 
 export class Content extends Component {
     constructor(props) {
       super(props)
+      this.state = {
+        isLoaded: false
+      }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true,
+            })
+        }, 2000)
     }
 
   render() {
@@ -16,15 +28,9 @@ export class Content extends Component {
                 <h1>My Photos</h1>
             </div>
             <div className={css.SearchResults}>
-                {/* {savedPosts.map((post) => {
-                    return <div className={css.SearchItem} key={post.title}>
-                        <p>{post.title}</p>
-                        <p>{post.name}</p>
-                        <img src={post.image} alt="random"/>
-                        <p>{post.discription}</p>
-                    </div>
-                })} */}
-                <PostItem savedPosts={savedPosts} />
+                {
+                    this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> : <Loader />
+                }            
             </div>
         </div>
     )
